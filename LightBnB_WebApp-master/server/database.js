887cc8e43bc4dacf33dbdd4ee6bf20c00fb89836
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-22 17:38:36 +0000

    update search feature - can use search parameters

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -122,12 +123,78 @@ exports.getAllReservations = getAllReservations;
  */
  const getAllProperties = (options, limit = 10) => {
 
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  const queryParams = [];
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    if (options.city) {
+      queryString += ` AND owner_id = $${queryParams.length}`
+    } else {
+      queryString += `WHERE owner_id = $${queryParams.length}`
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    if (options.city || options.owner_id) {
+      queryString += ` AND cost_per_night > $${queryParams.length}`
+    } else {
+      queryString += `WHERE cost_per_night > $${queryParams.length}`
+    }
+  } 
+  
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    if (options.city || options.owner_id || options.minimum_price_per_night) {
+      queryString += ` AND cost_per_night < $${queryParams.length}`
+    } else {
+      queryString += `WHERE cost_per_night < $${queryParams.length}`
+    }
+  } 
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `
+    GROUP BY properties.id
+    HAVING AVG(property_reviews.rating) >= $${queryParams.length}` 
+  } 
+
+
+  queryParams.push(limit);
+
+  if (options.minimum_rating) {
+    queryString += ` 
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+
+  } else {
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+    
+  }
+
+  console.log('queryString:', queryString)
+  console.log('queryParams:', queryParams)
+
   return pool
-    .query(
-      `SELECT * FROM properties LIMIT $1;`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows
-
     })
     .catch((err) => {
       return err.message;
